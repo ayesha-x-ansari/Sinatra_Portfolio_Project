e989a6c9421e5d2de3a705eda0d0526082101342
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-06-01 15:29:36 +0000

    cleaned user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,60 +1,21 @@
 class UsersController < ApplicationController
 
-  get '/reset' do
-    erb :'users/reset'
-  end
-
-  post '/reset' do
-    "ddddddd"
-    if params[:email] == "" || params[:password] == ""
-        flash[:message] = "All fields should be filled"
-        redirect to '/reset'
-    end
-      user = User.find_by_email(params[:email])
-      user.password = params[:password]
-      user.save
-  #    erb :show
-
-    #  user = User.find_by(:email => params[:email])
-    #  if !user.nil?
-    #   @user = User.update(:password => params[:password])
-    #   session[:user_id] = @user.id
-    #   redirect("/login")
-    #   end
-    end
-
-  get '/users/:id' do
-    if !logged_in?
-      redirect '/bags'
-    end
-
-    @user = User.find(params[:id])
-    if !@user.nil? && @user == current_user
-      erb :'users/show'
-    else
-      redirect '/bags'
-    end
+  get '/' do
+      erb :home
   end
 
   get '/signup' do
     if !session[:user_id]
       erb :'users/new'
     else
-      redirect to '/clubs'
+      redirect to '/users/home'
     end
   end
 
   post '/signup' do
-
-    if params[:name] == "" || params[:email] == ""  || params[:username] == "" || params[:password] == ""
+    if params[:name] == "" || params[:email] == ""  || params[:password] == ""
       flash[:message] = "All fields should be filled"
       redirect to '/signup'
-    end
-
-    user = User.find_by(:username => params[:username])
-    if !user.nil?
-      flash[:message] = "A account already exists with this username"
-      redirect("/signup")
     end
 
     user = User.find_by(:email => params[:email])
@@ -64,27 +25,23 @@ class UsersController < ApplicationController
     end
 
     if params[:password].size < 5
-      flash[:message] = "Password cannot be less than 8 characters"
+      flash[:message] = "Password cannot be less than 5 characters"
       redirect("/signup")
     end
 
-    @user = User.create(:name => params[:name], :email => params[:email], :username => params[:username], :password => params[:password])
+    @user = User.create(:name => params[:name], :email => params[:email], :password => params[:password])
     session[:user_id] = @user.id
-    redirect '/login'
+    redirect '/users/home'
   end
-
-
 
   get '/login' do
     @error_message = params[:error]
     if !session[:user_id]
       erb :'users/login'
     else
-      erb :'users/login'
-    #  redirect '/bags'
+      erb :'users/home'
     end
   end
-
 
   post '/login' do
     if params[:email] == ""  || params[:password] == ""
@@ -98,16 +55,42 @@ class UsersController < ApplicationController
       redirect("/login")
     end
 
-    user = User.find_by(:email => params[:email])
-    if user && user.authenticate(params[:password])
-  #  if user && user.password  == params[:password]
-      session[:user_id] = user.id
-      #redirect "/bags"
-      "ddddddddddd"
+    @user = User.find_by(:email => params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :'/users/home'
     else
       redirect to '/reset'
     end
+  end
 
+  get '/reset' do
+    erb :'users/reset'
+  end
+
+  post '/reset' do
+    if params[:email] == "" || params[:password] == ""
+      flash[:message] = "All fields should be filled"
+      redirect to '/reset'
+    end
+      user = User.find_by_email(params[:email])
+      user.password = params[:password]
+      user.save
+      session[:user_id] = user.id
+      redirect "/users/home"
+      end
+
+  get '/users/:id' do
+    if !logged_in?
+      redirect '/bags'
+    end
+
+    @user = User.find(params[:id])
+    if !@user.nil? && @user == current_user
+      erb :'users/show'
+    else
+      redirect '/bags'
+    end
   end
 
   get '/logout' do

50186aa387a1183d0f62697a50a0dde2512e77f2
ayesha-x-ansari <33658109+ayesha-x-ansari@users.noreply.github.com>
2018-06-04 20:34:06 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-require 'rack-flash'
-class ApplicationController < Sinatra::Base
+	uire 'rack-flash'
+class A     licationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
@@ -25,8 +25,8 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-			User.find(session[:user_id])
-		end
+	User.find(session[:user_id])
+     end
   end
 
 end
